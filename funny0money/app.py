cc98d2c3c1958068c5f00c9c015b2b08e515deaa
Exoen144 <coleenbai144@gmail.com>
2020-08-06 15:32:18 +0800

    hashing def dbinit

--- funny0money/app.py
+++ funny0money/app.py
@@ -74,22 +74,22 @@ def handle_message(event):
 
 
 def db_initiate():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+ #   creds = None
+ #   if os.path.exists('token.pickle'):
+  #      with open('token.pickle', 'rb') as token:
+   #         creds = pickle.load(token)
+   #     if creds and creds.expired and creds.refresh_token:
+    #        creds.refresh(Request())
+    #    else:
+    #        flow = InstalledAppFlow.from_client_secrets_file(
+   #             'credentials.json', SCOPES)
+    #        creds = flow.run_local_server(port=0)
+   #     with open('token.pickle', 'wb') as token:
+    #        pickle.dump(creds, token)
 
-    service = build('sheets', 'v4', credentials=creds)    
+    #service = build('sheets', 'v4', credentials=creds)    
 
-    sheet = service.spreadsheets()
+    #sheet = service.spreadsheets()
 
 def db_call():
     
