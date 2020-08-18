bc37bd82ec35d3e2d8df739629b62901671c4d10
Tony0919 <lamigo.det12345@yahoo.com>
2020-08-18 17:53:36 +0800

    test0818

--- funny0money/app.py
+++ funny0money/app.py
@@ -9,6 +9,8 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -39,22 +41,62 @@ def callback():
 
     return 'OK'
 
+def get_exchangeRate():
+   numb= []
+   cate=[]
+   data=[]
+   url_1= "https://rate.bot.com.tw/xrt?Lang=zh-TW"
+   resp_1 = requests.get(url_1)
+   ms = BeautifulSoup(resp_1.text,"html.parser")
+
+   t1=ms.find_all("td","rate-content-cash text-right print_hide")
+   for child in t1:
+      numb.append(child.text.strip())
+   
+   buy=numb[0:37:2]
+   sell=numb[1:38:2]
+
+   t2=ms.find_all("div","hidden-phone print_show")
+   for child in t2:
+      cate.append(child.text.strip())
+   for i in range(19):
+      data.append([cate[i] +'買入：'+buy[i]+ '賣出：'+sell[i]])
+
+   return data
+
+
+def get_movie():
+    movies = []
+    url_1= "https://movies.yahoo.com.tw/chart.html"
+    resp_1 = requests.get(url_1)
+    ms = BeautifulSoup(resp_1.text,"html.parser")
+
+    ms.find_all("div","rank_txt")
+    movies.append(ms.find('h2').text)
+
+    for rank_txt in ms.find_all("div","rank_txt"):
+        movies.append(rank_txt.text.strip())
+
+    return movies
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    count=0
-    while True:
         
-        input_text = event.message.text
-        if eval(input_text)>0 and eval(input_text)<=100000:
-            output_text= "記帳成功"
-            count=count+eval(input_text)
-        else:
-            output_text="記帳不成功"
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(str(count)))
+    input_text = event.message.text
+    if (eval(input_text)>0) and (eval(input_text)<=100000):
+        output_text= input_text
+    elif  input_text =="0":
+        hot_movie=get_movie()
+        output_text=hot_movie
+    elif input_text =="1":
+        exchangeRate=get_exchangeRate()
+        output_text=exchangeRate
+    else:
+        output_text="早安您好"
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(str(output_text)))
 
 
     
@@ -62,4 +104,3 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run()
-    
