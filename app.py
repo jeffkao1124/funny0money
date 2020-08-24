b533c6bd642e2ad2192e49bc21060f814cdfdd83
tzucheng <f84066024@gs.ncku.edu.tw>
2020-08-25 03:50:24 +0800

    splitupdate 2.0

--- app.py
+++ app.py
@@ -221,9 +221,10 @@ def handle_message(event):
         
     else:
         if (history_list[0]['Status'] == 'set') and ('分帳設定' in input_text):
+            selfId = history_list[0]['user_id']
             selfGroupId = history_list[0]['group_id']
             data_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId).filter(usermessage.user_id==selfId).filter(usermessage.status=='set').delete()
-            data_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId).filter(usermessage.status=='set')
+            data_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId).filter(usermessage.status=='set')#會連新資料一起刪掉
             #history_dic = {}
             #history_list = []
             count=0
