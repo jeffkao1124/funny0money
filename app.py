4dd2953bda0b9174053f44843ff1726433503db9
tzucheng <f84066024@gs.ncku.edu.tw>
2020-08-26 13:56:59 +0800

    add memberList

--- app.py
+++ app.py
@@ -258,6 +258,13 @@ def handle_message(event):
                 event.reply_token,
                 TextSendMessage(text= str(output_text)))
 
+        elif input_text == '分帳設定查詢':
+            groupMember=get_groupPeople(history_list,2)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text= str( groupMember )))
+
+
         elif ('結算' in input_text):
             selfGroupId = history_list[0]['group_id']
             data_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId ).filter(usermessage.status=='save')
