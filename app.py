7d5e53cf5d8ab27cb039e49a51430f37bf9067fd
tzucheng <f84066024@gs.ncku.edu.tw>
2020-08-27 11:26:01 +0800

    deleteLast

--- app.py
+++ app.py
@@ -233,6 +233,23 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text= str(output_text)))
+        elif input_text =='delete':
+            selfId = history_list[0]['user_id']
+            data_UserData = usermessage.query.filter(usermessage.user_id==selfId).filter(usermessage.status=='save').filter(usermessage.type=='user')
+            history_dic = {}
+            history_list = []
+            count=0
+            for _data in data_UserData:
+                count+=1
+                history_dic['Mesaage'] = _data.message
+                history_dic['Account'] = _data.account
+                history_list.append(history_dic)
+                history_dic = {}
+            data_UserData = usermessage.query.filter(usermessage.user_id==selfId).filter(usermessage.status=='save').get(count).delete()
+            output_text='刪除成功'
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text= str(output_text)))
         elif input_text =='help':
             help_text='1.記帳--輸入：記帳 項目 金額'+'\n'+'ex：記帳 麥當勞 200'+'\n'+'2.查帳--輸入：查帳'+'\n'+'3.刪除--輸入：刪除'
             line_bot_api.reply_message(
