ac35bd7590b8c45605ef31220b92117e047d6d27
Tony0919 <lamigo.det12345@yahoo.com>
2020-08-30 15:09:08 +0800

    formatJudgement(SpaceCheck)5

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def callback():
 
     if bodyjson['events'][0]['source']['type'] == 'group':
         receivedmsg = bodyjson['events'][0]['message']['text']
+        receivedmsg = receivedmsg.strip(' ')
         if '分帳設定' in receivedmsg:
             userName=receivedmsg.split(' ')[1]
             add_data = usermessage(
