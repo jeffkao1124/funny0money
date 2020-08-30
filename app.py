483c90a008c592b29ff985602c3eca9f12167d8b
Tony0919 <lamigo.det12345@yahoo.com>
2020-08-30 20:23:03 +0800

    joinGroup1

--- app.py
+++ app.py
@@ -200,7 +200,14 @@ def get_exchangeRate():
       
     return data
 
+@handler.add(JoinEvent)
+def handle_join(event):
+    newcoming_text = "謝謝邀請我這個機器來至此群組！！我會盡力為大家服務的～"
 
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextMessage(text=newcoming_text)
+        )
 
 def get_groupPeople(history_list,mode):
     selfId = history_list[0]['user_id']
