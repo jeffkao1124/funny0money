0923b67bca5d91279e9d812bde67b7fde95dcdca
Tony0919 <lamigo.det12345@yahoo.com>
2020-09-01 16:07:35 +0800

    back

--- app.py
+++ app.py
@@ -214,7 +214,34 @@ def get_history_list():
         history_dic = {}
     return history_list
 
-#記帳查帳
+def get_groupPeople(history_list,mode):
+    selfId = history_list[0]['user_id']
+    selfGroupId = history_list[0]['group_id']
+    SetMsgNumber = usermessage.query.filter(usermessage.group_id==selfGroupId).filter(usermessage.status=='set').count()
+    data_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId).filter(usermessage.status=='set')
+    history_dic = {}
+    history_list = []
+    for _data in data_UserData:
+        history_dic['nickname'] = _data.nickname
+        history_list.append(history_dic)
+        history_dic = {}
+    final_list =[]
+    for i in range(SetMsgNumber):
+        final_list.append(str(history_list[i]['nickname']))
+    count=0
+    new_list = []
+    for i in final_list:
+        if not i in new_list:
+            new_list.append(i)
+            count+=1
+
+    if mode ==1:
+        return count
+    elif mode ==2:
+        return new_list
+    else:
+        return 1
+
 def get_accountList():
     history_list = get_history_list()
     selfId = history_list[0]['user_id']
@@ -244,13 +271,13 @@ def get_accountList():
     perfect_list = perfect_list+'\n'+'累計花費:'+str(add)
     return perfect_list
 
-#分帳查帳
 def get_settleList():
     history_list = get_history_list()
     selfGroupId = history_list[0]['group_id']
     dataSettle_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId ).filter(usermessage.status=='save').filter(usermessage.type=='group')
     historySettle_dic = {}
     historySettle_list = []
+    #person_list  = get_groupPeople(history_list,2)
     for _data in dataSettle_UserData:
         historySettle_dic['Mesaage'] = _data.message
         historySettle_dic['Account'] = _data.account
@@ -266,127 +293,6 @@ def get_settleList():
     for j in range(count):
         perfect_list=perfect_list+str(j+1)+'.'+str(final_list[j])+'\n'
     return perfect_list
-
-#群組人數/名單
-def get_groupPeople(history_list,mode):
-    selfId = history_list[0]['user_id']
-    selfGroupId = history_list[0]['group_id']
-    SetMsgNumber = usermessage.query.filter(usermessage.group_id==selfGroupId).filter(usermessage.status=='set').count()
-    data_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId).filter(usermessage.status=='set')
-    history_dic = {}
-    history_list = []
-    for _data in data_UserData:
-        history_dic['nickname'] = _data.nickname
-        history_list.append(history_dic)
-        history_dic = {}
-    final_list =[]
-    for i in range(SetMsgNumber):
-        final_list.append(str(history_list[i]['nickname']))
-    count=0
-    new_list = []
-    for i in final_list:
-        if not i in new_list:
-            new_list.append(i)
-            count+=1
-
-    if mode ==1:
-        return count
-    elif mode ==2:
-        return new_list
-    else:
-        return 1
-
-#結算
-def settle()
-    time.sleep(2)
-    history_list = get_history_list()
-    selfGroupId = history_list[0]['group_id']
-    dataSettle_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId ).filter(usermessage.status=='save').filter(usermessage.type=='group')
-    historySettle_dic = {}
-    historySettle_list = []
-    person_list  = get_groupPeople(history_list,2)
-    count=0
-    for _data in dataSettle_UserData:
-        count+=1
-        historySettle_dic['Mesaage'] = _data.message
-        historySettle_dic['Account'] = _data.account
-        historySettle_dic['GroupPeople'] =_data.group_num
-        historySettle_list.append(historySettle_dic)
-        historySettle_dic = {}
-            
-    dataNumber=len(historySettle_list)
-    Zero= np.zeros((dataNumber,get_groupPeople(history_list,1)))
-    for i in range(dataNumber):
-        b=dict(historySettle_list[i])
-        GroupPeopleString=b['GroupPeople'].split(' ')
-        for j in range(1,len(GroupPeopleString),1):
-            if GroupPeopleString[0] == GroupPeopleString[j]:
-                del GroupPeopleString[j]
-                break
-        payAmount=int(b['Account'])/len(GroupPeopleString)
-        a1=set(get_groupPeople(history_list,2))
-        a2=set(GroupPeopleString)
-        duplicate = list(a1.intersection(a2))
-        count=0
-        for j in range(len(duplicate)):
-            place=get_groupPeople(history_list,2).index(duplicate[count])
-            Zero[i][place]=payAmount
-            count+=1
-
-    replaceZero=Zero
-    totalPayment=replaceZero.sum(axis=0)
-
-    paid= np.zeros((1,len(get_groupPeople(history_list,2))))
-    for i in range(len(get_groupPeople(history_list,2))):
-        for j in range(len(historySettle_list)):
-            b=dict(historySettle_list[j])
-            GroupPeopleString=b['GroupPeople'].split(' ')
-            if GroupPeopleString[0] == get_groupPeople(history_list,2)[i]:
-                paidAmount=int(b['Account'])
-                paid[0][i]=paid[0][i]+paidAmount
-            else:
-                continue
-
-    account=paid-totalPayment
-
-    #將人和錢結合成tuple，存到一個空串列
-    person_account=[]
-    for i in range(len(person_list)):
-        zip_tuple=(person_list[i],account[0][i])
-        person_account.append(zip_tuple)
-
-
-    #重複執行交換動作
-    result=""
-    for i in range(len(person_list)-1):
-        #排序
-        person_account=sorted(person_account, key = lambda s:s[1])
-
-        #找到最大、最小值
-        min_tuple=person_account[0]
-        max_tuple=person_account[-1]
-        min=float(min_tuple[1])
-        max=float(max_tuple[1])
-
-        #交換，印出該付的錢
-        if min==0 or max==0:
-                    pass
-        elif (min+max)>0:
-            result=result+str(min_tuple[0])+'付給'+str(max_tuple[0])+str(abs(round(min,2)))+'\n'
-            max_tuple=(max_tuple[0],min+max)
-            min_tuple=(min_tuple[0],0)
-        elif (min+max)<0:
-            result=result+str(min_tuple[0])+'付給'+str(max_tuple[0])+str(abs(round(max,2)))+'\n'
-            min_tuple=(min_tuple[0],min+max)
-            max_tuple=(max_tuple[0],0)
-        else:
-            result=result+str(min_tuple[0])+'付給'+str(max_tuple[0])+str(abs(round(max,2)))+'\n'
-            min_tuple=(min_tuple[0],0)
-            max_tuple=(max_tuple[0],0)
-        person_account[0]=min_tuple
-        person_account[-1]=max_tuple
-
-    return result
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
@@ -587,10 +493,97 @@ def handle_message(event):
                 TextSendMessage(text= str(output_text)))
 
         elif ('結算' in input_text):
-            output_text = settle()
+            time.sleep(2)
+            selfGroupId = history_list[0]['group_id']
+            dataSettle_UserData = usermessage.query.filter(usermessage.group_id==selfGroupId ).filter(usermessage.status=='save').filter(usermessage.type=='group')
+            historySettle_dic = {}
+            historySettle_list = []
+            person_list  = get_groupPeople(history_list,2)
+            count=0
+            for _data in dataSettle_UserData:
+                count+=1
+                historySettle_dic['Mesaage'] = _data.message
+                historySettle_dic['Account'] = _data.account
+                historySettle_dic['GroupPeople'] =_data.group_num
+                historySettle_list.append(historySettle_dic)
+                historySettle_dic = {}
+            
+            dataNumber=len(historySettle_list)
+            Zero= np.zeros((dataNumber,get_groupPeople(history_list,1)))
+            for i in range(dataNumber):
+                b=dict(historySettle_list[i])
+                GroupPeopleString=b['GroupPeople'].split(' ')
+                for j in range(1,len(GroupPeopleString),1):
+                    if GroupPeopleString[0] == GroupPeopleString[j]:
+                        del GroupPeopleString[j]
+                        break
+                payAmount=int(b['Account'])/len(GroupPeopleString)
+                a1=set(get_groupPeople(history_list,2))
+                a2=set(GroupPeopleString)
+                duplicate = list(a1.intersection(a2))
+                count=0
+                for j in range(len(duplicate)):
+                    place=get_groupPeople(history_list,2).index(duplicate[count])
+                    Zero[i][place]=payAmount
+                    count+=1
+
+            replaceZero=Zero
+            totalPayment=replaceZero.sum(axis=0)
+
+            paid= np.zeros((1,len(get_groupPeople(history_list,2))))
+            for i in range(len(get_groupPeople(history_list,2))):
+                for j in range(len(historySettle_list)):
+                    b=dict(historySettle_list[j])
+                    GroupPeopleString=b['GroupPeople'].split(' ')
+                    if GroupPeopleString[0] == get_groupPeople(history_list,2)[i]:
+                        paidAmount=int(b['Account'])
+                        paid[0][i]=paid[0][i]+paidAmount
+                    else:
+                        continue
+
+            account=paid-totalPayment
+
+            #將人和錢結合成tuple，存到一個空串列
+            person_account=[]
+            for i in range(len(person_list)):
+                zip_tuple=(person_list[i],account[0][i])
+                person_account.append(zip_tuple)
+
+
+            #重複執行交換動作
+            result=""
+            for i in range(len(person_list)-1):
+                #排序
+                person_account=sorted(person_account, key = lambda s:s[1])
+
+                #找到最大、最小值
+                min_tuple=person_account[0]
+                max_tuple=person_account[-1]
+                min=float(min_tuple[1])
+                max=float(max_tuple[1])
+
+                #交換，印出該付的錢
+                if min==0 or max==0:
+                    pass
+                elif (min+max)>0:
+                    result=result+str(min_tuple[0])+'付給'+str(max_tuple[0])+str(abs(round(min,2)))+'\n'
+                    max_tuple=(max_tuple[0],min+max)
+                    min_tuple=(min_tuple[0],0)
+                elif (min+max)<0:
+                    result=result+str(min_tuple[0])+'付給'+str(max_tuple[0])+str(abs(round(max,2)))+'\n'
+                    min_tuple=(min_tuple[0],min+max)
+                    max_tuple=(max_tuple[0],0)
+                else:
+                    result=result+str(min_tuple[0])+'付給'+str(max_tuple[0])+str(abs(round(max,2)))+'\n'
+                    min_tuple=(min_tuple[0],0)
+                    max_tuple=(max_tuple[0],0)
+                person_account[0]=min_tuple
+                person_account[-1]=max_tuple
+
+
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text= str(output_text)))
+                TextSendMessage(text= str(result)))
                 
         elif '快速選單' in input_text :
              line_bot_api.reply_message(  
