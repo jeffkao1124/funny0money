9f176670e52e8ee29e5fc462e17b2517dfb0fbce
Tony0919 <lamigo.det12345@yahoo.com>
2020-09-03 10:43:15 +0800

    deleteRepeatSpace3

--- app.py
+++ app.py
@@ -62,6 +62,13 @@ def callback():
                     birth_date = datetime.fromtimestamp(int(bodyjson['events'][0]['timestamp'])/1000)
                 )
         elif ('分帳' in receivedmsg)  and (len(re.findall(r" ",receivedmsg)) >= 3):
+            '''
+            split_1=receivedmsg.split(' ')[1]
+            chargeName=split_1.split(' ')[0]
+            split_2=split_1.split(' ')[1]
+            chargeNumber=split_2.split(' ')[0]
+            chargePeople=split_2.split(' ')[1]
+            '''
             chargeName=receivedmsg.split(' ',3)[1]
             chargeNumber=receivedmsg.split(' ',3)[2]
             chargePeople=receivedmsg.split(' ',3)[3]
@@ -307,6 +314,7 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text= str(output_text)))
+                
         elif input_text =='查帳':
             output_text = get_accountList()
             line_bot_api.reply_message(
