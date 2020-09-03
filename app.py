b1f9cc546062318df8d2caa46eb1b5b5e95f98ed
Tony0919 <lamigo.det12345@yahoo.com>
2020-09-03 17:43:42 +0800

    usermessage3

--- app.py
+++ app.py
@@ -379,7 +379,7 @@ def handle_message(event):
                 personID=history_dic['id']
                 data_UserData = usermessage.query.filter(usermessage.id==personID).delete(synchronize_session='fetch')
                 output_text='刪除成功'+'\n\n'+'記帳清單：'+'\n'+get_accountList(selfId)
-                usermessage.flush()
+                usermessage.commit()
                 # db.session.delete(data_UserData)
                 # db.session.commit()
                 # test1_dic={}
