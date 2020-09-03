24234dddd91399c7e89d51a78bd529f4bc3e030f
Tony0919 <lamigo.det12345@yahoo.com>
2020-09-03 11:35:47 +0800

    addTimeSleep3

--- app.py
+++ app.py
@@ -378,7 +378,6 @@ def handle_message(event):
                     history_list.append(history_dic)
                 personID=history_dic['id']
                 data_UserData = usermessage.query.filter(usermessage.id==personID).delete()
-                time.sleep(2)
                 output_text='刪除成功'+'\n\n'+'記帳清單：'+'\n'+get_accountList()
 
             line_bot_api.reply_message(
