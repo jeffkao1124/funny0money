c7ee7a141f696a29c3065763cc7a49c778fe7b95
max <410011max@gmail.com>
2020-09-12 20:15:54 +0800

    代墊者分開1

--- app.py
+++ app.py
@@ -537,8 +537,27 @@ def handle_message(event):
                 historySettle_dic['GroupPeople'] =_data.group_num
                 historySettle_list.append(historySettle_dic)
                 historySettle_dic = {}
+            
+            dataNumber=len(historySettle_list)
+            Zero= np.zeros((dataNumber,get_groupPeople(history_list,1)))
+            for i in range(dataNumber):
+                b=dict(historySettle_list[i])
+                GroupPeopleString=b['GroupPeople'].split(' ')  #刪除代墊者
+                del GroupPeopleString[0]
+                payAmount=int(b['Account'])/len(GroupPeopleString)
+                a1=set(get_groupPeople(history_list,2))      #分帳設定有的人
+                a2=set(GroupPeopleString)
+                duplicate = list(a1.intersection(a2))                     #a1和a2重複的人名
+                count=0
+                for j in range(len(duplicate)):      #分帳金額
+                    place=get_groupPeople(history_list,2).index(duplicate[count])
+                    Zero[i][place]=payAmount
+                    count+=1
 
-            paid= np.zeros((1,len(get_groupPeople(history_list,2))))  #代墊矩陣
+            replaceZero=Zero
+            totalPayment=replaceZero.sum(axis=0)
+
+            paid= np.zeros((1,len(get_groupPeople(history_list,2))))  #代墊金額
             for i in range(len(get_groupPeople(history_list,2))):
                 for j in range(len(historySettle_list)):
                     b=dict(historySettle_list[j])
@@ -546,23 +565,9 @@ def handle_message(event):
                     if GroupPeopleString[0] == get_groupPeople(history_list,2)[i]:
                         paidAmount=int(b['Account'])
                         paid[0][i]=paid[0][i]+paidAmount
+                    else:
+                        continue
 
-            dataNumber=len(historySettle_list)   #分帳矩陣
-            Zero= np.zeros((dataNumber,get_groupPeople(history_list,1)))
-            for i in range(dataNumber):
-                b=dict(historySettle_list[i])
-                GroupPeopleString=b['GroupPeople'].split(' ')
-                del GroupPeopleString[0] #刪除代墊者
-                payAmount=int(b['Account'])/len(GroupPeopleString)
-                a1=set(get_groupPeople(history_list,2))      #分帳設定有的人
-                a2=set(GroupPeopleString)
-                duplicate = list(a1.intersection(a2))                     #a1和a2重複的人名
-                for j in range(len(duplicate)):     #分帳金額
-                    place=get_groupPeople(history_list,2).index(duplicate[count])
-                    Zero[i][place]=payAmount
-
-            replaceZero=Zero
-            totalPayment=replaceZero.sum(axis=0)
             account=paid-totalPayment
 
             #將人和錢結合成tuple，存到一個空串列
