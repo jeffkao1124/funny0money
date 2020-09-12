0392d1889dd7c50581a6ac6d0014cca5ebc1e55b
tzucheng <f84066024@gs.ncku.edu.tw>
2020-09-12 11:15:24 +0800

    分帳紀錄時間

--- app.py
+++ app.py
@@ -263,7 +263,8 @@ def get_settleList():
     count=0
     for i in range(len(historySettle_list)):
         count+=1
-        final_list.append(str(historySettle_list[i]['Mesaage'])+' '+str(historySettle_list[i]['Account'])+' '+str(historySettle_list[i]['GroupPeople'])+str(historySettle_list[i]['Time']))
+        settleTime = str(historySettle_list[i]['Time'])
+        final_list.append(msgTime[:10]+' '+str(historySettle_list[i]['Mesaage'])+' '+str(historySettle_list[i]['Account'])+' '+str(historySettle_list[i]['GroupPeople']))
     perfect_list=''
     for j in range(count):
         perfect_list=perfect_list+str(j+1)+'.'+str(final_list[j])+'\n'
