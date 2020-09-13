1982813335e980ff518e8ddf63e8c74a785f1b4e
max <410011max@gmail.com>
2020-09-13 22:47:36 +0800

    2

--- app.py
+++ app.py
@@ -66,7 +66,9 @@ def callback():
                     user_id = bodyjson['events'][0]['source']['userId'], 
                     message = bodyjson['events'][0]['message']['text'], 
                     birth_date = datetime.fromtimestamp(int(bodyjson['events'][0]['timestamp'])/1000) 
-                ) 
+                )
+                db.session.add(add_data)
+                db.session.commit()
         elif ('分帳' in receivedmsg)  and (len(re.findall(r" ",receivedmsg)) >= 3):           
             chargeName=receivedmsg.split(' ',3)[1]
             chargeNumber=receivedmsg.split(' ',3)[2]
