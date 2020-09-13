e6f4386d6356e4aa4173c2a5401b85e43c6e9cb1
Wuborjenn <F84071281@ncku.edu.tw>
2020-09-13 13:48:56 +0800

    ==

--- app.py
+++ app.py
@@ -747,7 +747,6 @@ def handle_message(event):
                             )
                         )
             line_bot_api.reply_message(event.reply_token,Carousel_template)
-        elif 'test' in input_text :
             
         elif (eval(input_text)>0) and (eval(input_text)<=100000):
             output_text= input_text
