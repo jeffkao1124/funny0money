8fff5619e26d4a4347fccfcaa15e2d376f626f80
Wuborjenn <F84071281@ncku.edu.tw>
2020-09-15 22:01:16 +0800

    help

--- app.py
+++ app.py
@@ -758,7 +758,7 @@ def handle_message(event):
             ),
             URIImagemapAction(
                 #記錄分帳
-                link_uri="https://liff.line.me/1654876504-9wWz",
+                text="help",
                 area=ImagemapArea(
                     x=1000, y=0, width=1000, height=1000
                 )
