5dbf0adba79dbfcf93b122acf4823d199b4e0a54
Wuborjenn <F84071281@ncku.edu.tw>
2020-09-17 17:24:22 +0800

    ==

--- app.py
+++ app.py
@@ -810,7 +810,7 @@ def handle_message(event):
             ),
             MessageImagemapAction(
                 #使用說明
-                data="help",
+                text="help",
                 area=ImagemapArea(
                     x=530, y=555, width=420, height=235
                 )
