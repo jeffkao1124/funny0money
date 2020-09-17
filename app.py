f8e89a63ac74d7e95cc5d41334c992fa1887f803
max <410011max@gmail.com>
2020-09-17 23:12:17 +0800

    4

--- app.py
+++ app.py
@@ -804,10 +804,10 @@ def handle_message(event):
      
         elif input_text == '清空資料庫':
             data_UserData = usermessage.query.filter(usermessage.status=='None').delete(synchronize_session='fetch')
-            print(data_UserData)
+            print('第一筆'+str(data_UserData))
             sys.stdout.flush()
             data_UserData = usermessage.query.order_by(usermessage.birth_date).filter(usermessage.status=='USD' or usermessage.status=='JPY'  or usermessage.status=='EUR' )
-            print(data_UserData)
+            print('第二筆'+str(data_UserData))
             sys.stdout.flush()
             db.session.commit()
             line_bot_api.reply_message(
