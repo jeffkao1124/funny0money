ef54189f4651f0a8c1ed91bc924316fd8dacb185
Wuborjenn <F84071281@ncku.edu.tw>
2020-10-11 23:14:18 +0800

    ==

--- app.py
+++ app.py
@@ -924,9 +924,9 @@ def handle_message(event):
                     data_UserData = usermessage.query.filter(usermessage.id==personID).delete(synchronize_session='fetch')
                     db.session.add(add_data)
                     db.session.commit()
-                    output_text="刪除成功\n\n分帳名單:"
+                    output_text="刪除成功，若被刪除的人在帳目中，請記得把帳目也修改掉!\n\n分帳名單:"
             try:
-                if output_text=='刪除成功\n\n分帳名單:':
+                if output_text=='刪除成功，若被刪除的人在帳目中，請記得把帳目也修改掉!\n\n分帳名單:':
                     groupMember=get_groupPeople(history_list,2)
                     for i in range(get_groupPeople(history_list,1)):
                         output_text+='\n'+groupMember[i]
