324d624da65ae953dad94543cfe8991d0bbbaef7
jeffkao1124 <69136565+jeffkao1124@users.noreply.github.com>
2025-01-12 10:20:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1173,6 +1173,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage (output_text) )
         
 
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+#if __name__ == "__main__":
+#    app.run()
